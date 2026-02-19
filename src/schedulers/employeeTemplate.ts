137bd5cfbbb9cbfa8f78b9952d0be41407bcf07b
Suhaeri <suhaeriheri45@gmail.com>
2026-02-19 08:07:25 +0700

    meanmbahkan

--- src/schedulers/employeeTemplate.ts
+++ src/schedulers/employeeTemplate.ts
@@ -11,12 +11,12 @@ export const loadDeviceCache = async () => {
   const { rows } = await pgPool2.query(`
     SELECT
       "deviceId",
-      "deviceSN"
+      "deviceSN" AS "deviceSn"
     FROM "devices"
   `);
-
+    console.log(rows.map(r => [r.deviceSn, r.deviceId]))
   deviceCache = new Map(
-    rows.map(r => [r.deviceSN, r.deviceId])
+    rows.map(r => [r.deviceSn, r.deviceId])
   );
 
   lastLoadedAt = Date.now();
